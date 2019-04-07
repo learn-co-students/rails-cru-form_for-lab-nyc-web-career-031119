c5acb025446345fa33d2bd3a820dd877f31b0f75
J-Agens <johntagens@gmail.com>
2019-04-07 16:01:18 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+5.times do
+  Artist.create(name: Faker::Music.band, bio: Faker::Lorem.paragraph)
+end
+
+5.times do
+  Genre.create(name: Faker::Music.genre)
+end
+
+20.times do
+  Song.create(name: Faker::Music::Phish.song, artist_id: rand(Artist.all.first.id..(Artist.first.id + Artist.all.count)), genre_id: rand(Genre.all.first.id..(Genre.first.id + Genre.all.count)))
+end

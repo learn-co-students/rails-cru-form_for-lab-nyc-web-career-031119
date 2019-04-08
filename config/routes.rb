c983da127f68689e2d84a792321d297d271c6ab1
github username <github email address>
2019-04-08 09:55:08 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,32 @@
 Rails.application.routes.draw do
+  # get 'songs/index'
+  #
+  # get 'songs/show'
+  #
+  # get 'songs/new'
+  #
+  # get 'songs/edit'
+  #
+  # get 'genres/index'
+  #
+  # get 'genres/show'
+  #
+  # get 'genres/new'
+  #
+  # get 'genres/edit'
+  #
+  # get 'artists/index'
+  #
+  # get 'artists/show'
+  #
+  # get 'artists/new'
+  #
+  # get 'artists/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+resources :artists
+resources :genres
+resources :songs
+
 end
